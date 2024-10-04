1da46589ca581f8852004aece9dd3eea52837f2d
Monica Hernandez Ospina <167367921+Moni2502@users.noreply.github.com>
2024-10-04 17:18:33 -0500

    footer.php
    Modifique el footer cualquier cosa me comentan

--- includes/footer.php
+++ includes/footer.php
@@ -1,25 +1,66 @@
 <footer class="footer">
     <div class="footer-container">
-        <div class="footer-logo">
-            <!-- <img class="logo" src="/copapa/img/copapa.png" alt="Logo COPAPA"> -->
-        </div>
-        <!-- <div class="footer-nav">
-            <ul class="nav-links">
-                <li><a class="nav-link" href="privacy.php">Política de Privacidad</a></li>
-            </ul>
-        </div> -->
-        <div class="footer-social">
-            <a class="social-icon" href="#" target="_blank">
-                <img src="/Proyecto_gaes_copapa/Proyecto_gaes_copapa/img/facebook.png" alt="Facebook">
-            </a>
-            <a class="social-icon" href="#" target="_blank">
-                <img src="/Proyecto_gaes_copapa/Proyecto_gaes_copapa/img/x.png" alt="Twitter">
-            </a>
-            <a class="social-icon" href="#" target="_blank">
-                <img src="/Proyecto_gaes_copapa/Proyecto_gaes_copapa/img/instagram.png" alt="Instagram">
-            </a>
-        </div>
-        <a class="nav-link" href="privacy.php">Política de Privacidad</a>
+    <footer class="footer">
+    <div class="footer-logo">
+    <div class="container-logo">
+						 <img src="../img/copapa.png" alt="logo de la Pagina de COPAPA"
+						<h1 class="logo"><a href="/"></a></h1>
+					</div>
+			<div class="container container-footer">
+				<div class="menu-footer">
+					<div class="contact-info">
+						<p class="title-footer">Información de Contacto</p>
+						<ul>
+							<li>
+								Dirección: Boyacá.
+							</li>
+							<li>Teléfono: 1234567890</li>
+							<li>EmaiL: Copapa2023@support.com</li>
+						</ul>
+						<div class="social-icons">
+							<span class="facebook">
+								<i class="fa-brands fa-facebook-f"></i>
+							</span>
+							<span class="twitter">
+								<i class="fa-brands fa-twitter"></i>
+							</span>
+							<span class="instagram">
+								<i class="fa-brands fa-instagram"></i>
+							</span>
+						</div>
+					</div>
+
+					<div class="information">
+						<p class="title-footer">Información</p>
+						<ul>
+							<li><a href="#">Acerca de Nosotros</a></li>
+							<li><a href="#">Politicas de Privacidad</a></li>
+							<li><a href="#">Términos y condiciones</a></li>
+							<li><a href="#">Contactános</a></li>
+						</ul>
+					</div>
+
+					<div class="my-account">
+						<p class="title-footer">Mi cuenta</p>
+
+						<ul>
+							<li><a href="#">Mi cuenta</a></li>
+							<li><a href="#">Historial de ordenes</a></li>
+							<li><a href="#">Lista de deseos</a></li>
+							<li><a href="#">Reembolsos</a></li>
+						</ul>
+					</div>
+						</div>
+					</div>
+				</div>
+				</div>
+			</div>
+		</footer>
         <p class="footer-text">&copy; <?php echo date("Y"); ?> COPAPA. Todos los derechos reservados.</p>
     </div>
+		<script
+			src="https://kit.fontawesome.com/81581fb069.js"
+			crossorigin="anonymous"
+
+		></script>
 </footer>
